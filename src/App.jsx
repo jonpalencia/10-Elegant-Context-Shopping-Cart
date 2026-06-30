2dc9ac7220a3eac82830a2e58bcf16b7ca4e1caf
jonpalencia <jonpalencia20@gmail.com>
2026-07-01 05:44:50 +0800

    refactor: App.jsx to clean component and use the CartContextComponent wrapper with the useContext.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import CartContextComponent from './store/shopping-cart-context.jsx';
 import Header from './components/Header';
 import Shop from './components/Shop';
-import Product from './components/Product';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
 
 function App() {
   return (
     <CartContextComponent>
       <Header />
-      <Shop>
-        {DUMMY_PRODUCTS.map(product => (
-          <li key={product.id}>
-            <Product {...product} />
-          </li>
-        ))}
-      </Shop>
+      <Shop />
     </CartContextComponent>
   );
 }
