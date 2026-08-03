f2e4a5ac95994501c6de70ebf2b75c825b4dd09a
jonpalencia <jonpalencia20@gmail.com>
2026-08-04 05:50:58 +0800

    finalise: tweak app to finalise - only fix the module export to consistintly use name export for outsourcing calculation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import CartContextComponent from './store/shopping-cart-context.jsx';
+import { CartContextProvider } from './store/shopping-cart-context.jsx';
 import Header from './components/Header';
 import Shop from './components/Shop';
 
 function App() {
   return (
-    <CartContextComponent>
+    <CartContextProvider>
       <Header />
       <Shop />
-    </CartContextComponent>
+    </CartContextProvider>
   );
 }
 
